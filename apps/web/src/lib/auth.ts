8e1361ad84634e1b6bf053ed5e1791ffa2e2f2dd
Khalid <alhajr20@gmail.com>
2025-10-26 02:30:55 +0400

    adding nextjs setup

---
+++ apps/web/src/lib/auth.ts
@@ -0,0 +1,6 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+export const auth = betterAuth({
+    //...your config
+    plugins: [nextCookies()] // make sure this is the last plugin in the array
+})
